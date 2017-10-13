3570f7c49cb411c4070af56a4a1ccea55d002a53
alex <irazabal@us.ibm.com>
2017-10-12 21:57:54 -0400

    fixed path to addons

--- scripts/deploy-to-bluemix/deploy_prometheus_and_grafana.sh
+++ scripts/deploy-to-bluemix/deploy_prometheus_and_grafana.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "installing prometheus"
+echo "current directory `pwd`"
 kubectl apply -f ./addons/addons/prometheus.yaml
 echo "installing grafana"
 kubectl apply -f ./addons/addons//grafana.yaml
